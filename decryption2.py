5705c712df4ba5cb8b615466b3f154820733ca38
Thomas Schittina <thomas.schittina@uconn.edu>
2024-09-08 13:25:56 -0400

    solved problem 2

--- decryption2.py
+++ decryption2.py
@@ -5,3 +5,13 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+chars = list(encrypted_message)
+n = len(chars)
+
+for x in range(1, n//2, 2):
+    y = n - x - 1
+
+    chars[x], chars[y] = chars[y], chars[x]
+
+decrypted_message = ''.join(chars)
+print(decrypted_message)
